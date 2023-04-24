0d64a9ee3f29bf00574d83639689c991168e48a6
gourabb820 <91730250+gourabb820@users.noreply.github.com>
2023-04-24 18:46:29 +0530

    adding props from parent to child

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Test1 from './component/Test1.svelte';
+  let color = 'Red';
+  export let name = "Gourab"; //for passing props
+</script>
+
+<main>
+  <h1 class="title">Hello World</h1>
+  <h3>my color is  {color} </h3>
+  <Test1 name={name}/>
+</main>
+
+<style>
+.title {
+  color: red
+}
+</style>
---
+++ src/component/Test1.svelte
@@ -0,0 +1,14 @@
+<script>
+export let name ='Default'; // it will take props from parent otherwise default
+</script>
+
+<main>
+<p> Here is the test component</p>
+<h5>My name is {name} </h5>
+</main>
+
+<style>
+.test {
+  color: blue
+}
+</style>
