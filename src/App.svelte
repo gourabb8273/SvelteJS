d20afddbcf27a5526ff0853011e8e9ddd80a4b13
gourabb820 <91730250+gourabb820@users.noreply.github.com>
2023-04-24 19:12:44 +0530

    adding multiple props as an object from parent to child

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,18 @@
   import Test1 from './component/Test1.svelte';
   let color = 'Red';
   export let name = "Gourab"; //for passing props
+
+  export let userInfo={
+    firstName :"poto",
+    age: 26,
+    phone: 8695456235
+  } 
 </script>
 
 <main>
   <h1 class="title">Hello World</h1>
   <h3>my color is  {color} </h3>
-  <Test1 name={name}/>
+  <Test1 {...userInfo} name={name}/>
 </main>
 
 <style>
--- src/component/Test1.svelte
+++ src/component/Test1.svelte
@@ -1,10 +1,18 @@
 <script>
 export let name ='Default'; // it will take props from parent otherwise default
+
+//mutating props in this local
+name = "gourab banerjee"
+
+  export let firstName;
+  export let age;
+  export let phone;
 </script>
 
 <main>
 <p> Here is the test component</p>
 <h5>My name is {name} </h5>
+<h4>Here is {firstName} of age {age} and phone no is {phone}</h4>
 </main>
 
 <style>
