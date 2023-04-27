ccaaf80b5da6e6c6cfcb99d97441fe7d9c8995a7
gourabb820 <91730250+gourabb820@users.noreply.github.com>
2023-04-27 12:44:30 +0530

    adding special syntax $ to read store

--- src/component/UsedStore/DisplayCounter.svelte
+++ src/component/UsedStore/DisplayCounter.svelte
@@ -1,20 +1,22 @@
 <script>
   //this will show count in browser
+import { onDestroy } from 'svelte';
+
   import { count } from "../../stores/stores";
   import Decrement from "./Decrement.svelte";
   import Increment from "./Increment.svelte";
   import Reset from "./Reset.svelte";
-  let counter;
-  //subscribing global state count and assining the value to local variable counter
-  count.subscribe((value) => {
-    counter = value;
-  });
+  // let counter;
+  // //subscribing global state count and assining the value to local variable counter
+  // count.subscribe((value) => {
+  //   counter = value;
+  // });
 
 </script>
 
 <div>
 <h3>Counter using store</h3>
-<h2>current counter is {counter}</h2>
+<h2>current counter is {$count}</h2>
 <Increment />
 <Decrement />
 <Reset />
